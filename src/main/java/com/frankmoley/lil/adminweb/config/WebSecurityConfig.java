aa7b4d89af57898b2b01b2af29f687292e1f3761
Adeline <k4adel2020@gmail.com>
2023-03-31 14:55:31 +0600

    com

---
+++ src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.frankmoley.lil.adminweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/","/home").permitAll()
+                .antMatchers("/customers").hasAnyRole("USER","ADMIN",)
+                .antMatchers("/orders").hasRole("ADMIN")
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Bean
+    public UserDetailsService users(DataSource dataSource){
+       return new JdbcUserDetailsManager(dataSource);
+    }
+
+}
