994d5931d95aa851d9cabc698488feb5827245f2
Adeline <k4adel2020@gmail.com>
2023-03-31 14:59:25 +0600

    first commit

--- src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
+++ src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
                 .antMatchers("/","/home").permitAll()
-                .antMatchers("/customers").hasAnyRole("USER","ADMIN",)
+                .antMatchers("/customers").hasAnyRole("USER","ADMIN")
                 .antMatchers("/orders").hasRole("ADMIN")
                 .and()
                 .httpBasic();
